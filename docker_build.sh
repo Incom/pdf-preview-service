e011a828d88c9902224143cdd03c4ca6067d03c1
neomerx <info@neomerx.com>
2019-10-08 16:16:38 +0300

    Refactor docker building scripts.

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -n "$1" ]; then
+    docker build ./ -f ./docker/fpm.base//Dockerfile --tag=neomerx/ip2i-fpm-base
+    docker push neomerx/ip2i-fpm-base
+fi
+
 docker build ./ -f ./docker/fpm/Dockerfile --tag=neomerx/ip2i-service
 docker build ./ -f ./docker/nginx/Dockerfile --tag=neomerx/ip2i-web
-
-docker push neomerx/ip2i-service
-docker push neomerx/ip2i-web
---
+++ docker_push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker push neomerx/ip2i-fpm-base
+docker push neomerx/ip2i-service
+docker push neomerx/ip2i-web
