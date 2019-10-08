e2572fff6a616246dda42c9d7102c09f494d4087
neomerx <info@neomerx.com>
2019-10-08 11:07:04 +0300

    Add push to docker registry.

--- build_docker.sh
+++ build_docker.sh
@@ -2,3 +2,6 @@
 
 docker build ./ -f ./docker/fpm/Dockerfile --tag=neomerx/ip2i-service
 docker build ./ -f ./docker/nginx/Dockerfile --tag=neomerx/ip2i-web
+
+docker push neomerx/ip2i-service
+docker push neomerx/ip2i-web
